421fd6614f97ce0b265c546519be786aa1ee09de
NIKHIL0VERMA <nikhil2003verma@gmail.com>
2024-10-01 22:34:34 +0530

    Darshan Doot SIH setup

---
+++ darshan_doot/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Museum, Event, Ticket
+
+class MuseumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Museum
+        fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+        read_only_fields = ('ticket_id', 'booking_date', 'payment_status', 'total_amount', 'transaction_id', 'verification_code', 'stripe_payment_intent_id')
+
+    def create(self, validated_data):
+        ticket = Ticket.objects.create(**validated_data)
+        return ticket
